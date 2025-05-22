ab079072d5cdb41f7571e4d93f2ab9c5deda3292
aswintbbc <30798522+aswintbbc@users.noreply.github.com>
2025-05-22 15:09:20 +0530

    feat: update main app structure for Firebase integration; add firebase_ai and flutter_markdown dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
-import 'package:chaty/services/notification_services.dart';
-import 'package:fire_chat/screens/home_screen.dart';
+import 'package:firebase_ai/firebase_ai.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
-  await NotificationService().initialize();
+// REQUIRED if you want to run on Web
+const FirebaseOptions? options = null;
 
-  runApp(const MyApp());
+void main() {
+  runApp(const GenerativeAISample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GenerativeAISample extends StatelessWidget {
+  const GenerativeAISample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chaty Example',
+      title: 'Flutter + Vertex AI',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color.fromARGB(255, 171, 222, 244),
+        ),
+        useMaterial3: true,
       ),
-      home: const HomeScreen(), // Initial screen
+      home: const Text('Flutter + Vertex AI'),
     );
   }
 }
